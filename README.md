003ed805277e933f69eed1592a0a846729cb87a7
Afifah Khoirunnisa <afifahnisa041@gmail.com>
2024-05-27 07:37:13 +0700

    add fitur to md

--- README.md
+++ README.md
@@ -3,14 +3,27 @@
 |  |  |
 |--|--|
 | NIM | *2341720250* |
-| Nama Lengkap | *Afifah Khoirunnisa* |
+| Nama Lengkap | Afifah Khoirunnisa |
 | Kelas | TI-1B |
 | Dosen Pengampu | Erfan Rohadi, ST., M.Eng., Ph.D |
 | Dosen Pengampu | Mohammad Faried Rahmat, S.ST., M.Tr.T |
 
 # SISTEM OPERASI
-### Fitur
+## Fitur
 |  |  |  |
 |--|--|--|
-|*No| **Fitur* | *Detail* |
-| 1 | Membuat Direktori | Menggunakan perintah MKDIR |
+|No| Fitur | Detail |
+| 1 | Membuat Direktori | Menggunakan perintah 'mkdir nama_direktori' |
+| 2 | Melihat Direktori/file dalam Perangkat | Menggunakan perintah 'ls' untuk direktori dan file|
+| 3 | Membuat file baru | Pengguna diminta memasukkan nama file baru, kemudian file tersebut dibuka dengan teks editor Nano untuk pengeditan. |
+| 4 | Menghapus file | Menggunakan perintah 'rm nama_file' |
+| 5 | Mengganti nama file/direktori | Menggunakan perintah 'mv' |
+| 6 | Memeriksa koneksi internet | Menggunakan perintah 'ping' untuk tes koneksi|
+| 7 | Menampilkan informasi sistem | Menggunakan perintah uname -a' |
+| 8 | Menampilkan penggunaan disk | Menggunakan perintah 'df -h' untuk informasi disk |
+| 9 | Menampilkan penggunaan memori | Menggunakan perintah 'free -h' untuk informasi memori |
+| 10 | Menampilkan informasi CPU | Menggunakan perintah 'lscpu' untuk informasi CPU |
+
+
+# SCREENSHOOT PROGRAM
+<img src="./SS program.jpg">
---
+++ sisop.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# UAS - SISTEM OPERASI
+# Nama  : Afifah Khoirunnisa
+# NIM   : 2341720250
+# Kelas : TI -1B
+
+menu() {
+    echo "SELAMAT DATANG DI APLIKASI FILE MASTER"
+    echo "======================================"
+    echo "Pilih salah satu fitur berikut"
+    echo "- Management Direktori -"
+    echo "1. Membuat direktori baru"
+    echo "2. Melihat direktori dalam perangkat"
+    echo "3. Membuat file baru"
+    echo "4. Menghapus file"
+    echo "5. Mengganti nama file/direktori"
+    echo ""
+    echo "- Management Perangkat -"
+    echo "6. Memeriksa koneksi internet"
+    echo "7. Menampilkan informasi sistem"
+    echo "8. Menampilkan penggunaan disk"
+    echo "9. Menampilkan penggunaan memori"
+    echo "10. Menampilkan informasi CPU"
+    echo ""
+    echo "0. Keluar"
+}
+
+membuat_direktori_baru() {
+    read -p "Masukkan nama direktori baru: " dirnama
+    mkdir "$dirnama"
+    echo "Direktori '$dirnama' telah dibuat."
+    echo ""
+}
+
+menampilkan_isi_direktori() {
+    echo "Berikut adalah isi dari seluruh direktori: "
+    ls
+    echo ""
+}
+
+membuat_file_baru() {
+    read -p "Masukkan nama file baru: " filename
+    nano "$filename"
+    echo ""
+    if [ ! -f "$filename" ]; then
+        echo "File tidak disimpan. Membatalkan pembuatan file baru."
+    else
+        echo "File berhasil dibuat: $filename"
+    fi
+}
+
+menghapus_file() {
+    read -p "Masukkan nama file yang ingin dihapus: " filename
+    if [ -f "$filename" ]; then
+        rm "$filename"
+        echo "File '$filename' telah dihapus."
+    else
+        echo "File '$filename' tidak ditemukan."
+    fi
+    echo ""
+}
+
+mengganti_nama_file() {
+    read -p "Masukkan nama file/direktori yang ingin diubah namanya: " oldname
+    if [ -e "$oldname" ]; then
+        read -p "Masukkan nama file/direktori baru: " newname
+        mv "$oldname" "$newname"
+        echo "Nama file/direktori '$oldname' telah diubah menjadi '$newname'."
+    else
+        echo "File/direktori '$oldname' tidak ditemukan."
+    fi
+    echo ""
+}
+
+memeriksa_koneksi_internet() {
+    echo "Mengecek koneksi internet...."
+    if ping -c 4 google.com; then
+        echo ""
+        echo "Koneksi internet OK"
+    else
+        echo ""
+        echo "Koneksi internet gagal"
+    fi
+    echo ""
+}
+
+menampilkan_informasi_system() {
+    echo "Informasi sistem: "
+    uname -a
+    echo ""
+}
+
+menampilkan_penggunaan_disk() {
+    echo "Penggunaan disk: "
+    df -h
+    echo ""
+}
+
+menampilkan_penggunaan_memori() {
+    echo "Penggunaan memori:"
+    free -h
+    echo ""
+}
+
+menampilkan_cpu_info() {
+    echo "Informasi CPU:"
+    lscpu
+    echo ""
+}
+
+
+echo "Selamat Datang di Aplikasi File Master"
+echo "======================================"
+echo "Aplikasi ini akan membantu Anda membuat dan memanajemen file/direktori" 
+echo "menampilkan informasi mengenai perangkat Anda"
+echo ""
+
+while true; do
+    menu
+    read -p "Pilihan Anda: " choice
+    case $choice in
+        1) membuat_direktori_baru ;;
+        2) menampilkan_isi_direktori ;;
+        3) membuat_file_baru ;;
+        4) menghapus_file ;;
+        5) mengganti_nama_file ;;
+        6) memeriksa_koneksi_internet ;;
+        7) menampilkan_informasi_system ;;
+        8) menampilkan_penggunaan_disk ;;
+        9) menampilkan_penggunaan_memori ;;
+        10) menampilkan_cpu_info ;;
+        0) break ;;
+        *) echo "Pilihan tidak valid!" ;;
+    esac
+done
